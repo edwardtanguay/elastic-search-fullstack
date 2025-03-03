3b4c607327d670f7337d506080c224907e142562
Edward Tanguay (windows) <edward@tanguay.info>
2025-03-03 14:28:47 +0100

    info out

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from "./App.tsx";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import "./index.scss";
 import { PageWelcome } from "./pages/PageWelcome.tsx";
-import { PageInfo } from "./pages/PageInfo.tsx";
+
 import { PageAbout } from "./pages/PageAbout.tsx";
 import { Page404 } from "./pages/Page404.tsx";
 import { PageSearch } from "./pages/PageSearch.tsx";
@@ -20,10 +20,7 @@ const router = createBrowserRouter([
 				path: "/welcome",
 				element: <PageWelcome />,
 			},
-			{
-				path: "info",
-				element: <PageInfo />,
-			},
+
 			{
 				path: "about",
 				element: <PageAbout />,
